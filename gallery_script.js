a670930d7dfd5fe9bfd06a0a899c3a232ef73d69
ConnorLee2 <22938115+ConnorLee2@users.noreply.github.com>
2022-03-02 20:38:53 +0000

    test

--- gallery_script.js
+++ gallery_script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         var content = "";
         for (var i in products) {
             if (i % 3 === 0) {
-                $("#products").append("<tr id='row" + i + "'></tr>");
+                $("#apps").append("<tr id='row" + i + "'></tr>");
                 j = i;
             }
             $("#row" + j).append("<td><div><img src='" + products[i].image + "' style='width:130px;height:200px;'></div><div class='text-center'>" + products[i].name + " </br><div>" + products[i].price + "</div></div></td>");
