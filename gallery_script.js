3c1ef48bf2c342533bf92cbb91df4808da579c83
ConnorLee2 <22938115+ConnorLee2@users.noreply.github.com>
2022-03-02 20:37:12 +0000

    test

--- gallery_script.js
+++ gallery_script.js
@@ -1,34 +1,16 @@
 $(document).ready(function () {
-    $.getJSON('https://github.com/ConnorLee2/connorlee2.github.io/blob/main/assets/data.json', function (data) {
-        var apps = data;
+    $.getJSON('assets/data.json', function (data) {
+        var products = data;
+
         var j = 0;
-        var appStoreIconSvg = "https://upload.wikimedia.org/wikipedia/commons/6/67/App_Store_%28iOS%29.svg";
-        var playStoreIconSvg = "https://upload.wikimedia.org/wikipedia/commons/d/d0/Google_Play_Arrow_logo.svg";
-        var appStoreStyle = "";
-        var playStoreStyle = "";
-    
-        for (var i in apps) {
-            // Hide play store icon if no relevant link
-            if (apps[i].playStoreLink === "") {
-                playStoreStyle = "display: none;";
-            } else {
-                playStoreStyle = "";
-            }
-    
-            // Hide app store icon if no relevant link
-            if (apps[i].appStoreLink === "") {
-                appStoreStyle = "display: none;";
-            } else {
-                appStoreStyle = "";
-            }
-            // Create a row
+        var content = "";
+        for (var i in products) {
             if (i % 3 === 0) {
-                $("#apps").append("<div class='row pb-3' id='row" + i + "'></div>");
+                $("#products").append("<tr id='row" + i + "'></tr>");
                 j = i;
             }
-            // Add up to 3 items to a row
-            $("#row" + j).append("<div class='col-sm-4 pb-3'><div class='card px-2 py-2'><div class='card-body'><h3 class='card-title'>" +
-                apps[i].appName + "</h3><a href='" + apps[i].appStoreLink + "'><img width='32px' style='" + appStoreStyle + "'src='" + appStoreIconSvg + "'></a> <a href='" + apps[i].playStoreLink + "'><img width='32px' style='" + playStoreStyle + "'src='" + playStoreIconSvg + "'></a><p class='card-text text-primary'>" + "Year: " + apps[i].year + "<br >" + apps[i].technologies + "</p></div><img src='" + apps[i].image + "' class='card-img-top px-4 pt-2 pb-4' alt='...'></div></div>");
+            $("#row" + j).append("<td><div><img src='" + products[i].image + "' style='width:130px;height:200px;'></div><div class='text-center'>" + products[i].name + " </br><div>" + products[i].price + "</div></div></td>");
         }
     });
+
 });
